a9289679a945bb36354d48d95e05a0a1a68fe4fe
LeviMetDeB <LeviMetDeB>
2022-12-28 17:00:29 +0100

    Co-authored-by: studentThomas <studentThomas@users.noreply.github.com>

--- src/main/java/dmt/HomeUI.java
+++ src/main/java/dmt/HomeUI.java
@@ -1,46 +1,40 @@
 package dmt;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
 public class HomeUI extends Application {
-    public static void main(String[] args) {
-        System.out.println("Hallo");
-        launch(args);
-    }
 
-    public void start(Stage stage) {
-        ArrayList<Person> persons = PersonData.getPersons();
-        ScrollPane scrollPane = new ScrollPane();
 
-        VBox vBox = new VBox();
-        // for (int i = 0; i < 40; i++) {
-        // vBox.getChildren().add(new Button("Button " + i));
-        // }
-        for (Person person : persons) {
-            Button button = new Button();
-            button.setText(person.getName());
-            vBox.getChildren().add(button);
+    public void start(Stage Stage) {
+    Button PersonButton = new Button("Person");
+    TextField rightText = new TextField();
+    Button CourseButton = new Button("Course");
 
-            button.setOnAction((event) -> {
-                Stage personWindow = new Stage();
-                personWindow.setTitle(person.getName());
-                personWindow.show();
-                stage.close();
-            });
+    //this is how it will connect i think
+    // PersonButton.setOnAction((event) -> {
+    //     window.setScene(PersonUI);
+    // });
+    // CourseButton.setOnAction((event) -> {
+    //     window.setScene(CourseUI);
+    // });
+    HBox componentGroup = new HBox();
+    componentGroup.setSpacing(20);
+    componentGroup.getChildren().addAll(PersonButton, CourseButton);
 
-        }
+    Scene scene = new Scene(componentGroup);
 
-        scrollPane.setContent(vBox);
 
-        Scene scene = new Scene(scrollPane, 300, 250);
-        stage.setScene(scene);
-        stage.show();
+        Stage.setScene(scene);
+        Stage.show();
     }
 }
