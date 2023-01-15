644f746876cf7d942dbc83f8b0e6e1f8609cf937
LeviMetDeB <LeviMetDeB>
2023-01-15 13:53:41 +0100

    you can now add a bunch of stuff

--- src/main/java/dmt/UI/AddCertificate.java
+++ src/main/java/dmt/UI/AddCertificate.java
@@ -26,73 +26,71 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class AddEnrollment {
-
+public class AddCertificate {
+    
     public static Parent getView() {
         ArrayList<Person> persons = PersonData.getPersons();
         ScrollPane scrollPane = new ScrollPane();
         ScrollPane scrollPane2 = new ScrollPane();
         BorderPane layout = new BorderPane();
-      
         layout.setCenter(scrollPane);
-
+        
         VBox vBox = new VBox();
         VBox vBox2 = new VBox();
         ComboBox comboBox = new ComboBox();
         for (Person person : persons) {
             comboBox.getItems().add(person.getEmail());
         }
-
-        Label EmailAsk = new Label("Enter Email:");
-        TextField EmailField = new TextField();
-        Label RegistrationDateAsk = new Label("Enter Registration date");
-        TextField RegistrationDateField = new TextField();
-        Label IdAsk = new Label("Enter Id:");
-        TextField IdField = new TextField();
-        Label CertificateIdAsk = new Label("Enter CertificateId:");
-        TextField CertificateIdField = new TextField();
+        Label PersonEmailAsk = new Label("Enter email:");
+        Label CourseIdAsk = new Label("Enter Course Id:");
+        TextField CourseIdField = new TextField();
+        Label RegistrationAsk = new Label("Enter Registration Date:");
+        TextField RegistrationField = new TextField();
+        Label GradeAsk = new Label("Enter Grade");
+        TextField GradeField = new TextField();
+        Label TeacherAsk = new Label("Enter Teacher");
+        TextField TeacherField = new TextField();
         Button startButton = new Button("Insert Data");
-        vBox.getChildren().addAll(EmailAsk, comboBox, IdAsk, IdField, RegistrationDateAsk, RegistrationDateField, CertificateIdAsk, CertificateIdField, startButton);
+        vBox.getChildren().addAll(PersonEmailAsk, comboBox, CourseIdAsk, CourseIdField, RegistrationAsk, RegistrationField, GradeAsk, GradeField, TeacherAsk, TeacherField, startButton);
 
-        // collect inserted data
+        //collect inserted data
         startButton.setOnAction((event) -> {
             Object email = comboBox.getValue();
-            String registrationDate = RegistrationDateField.getText();
-            String id = IdField.getText();
-            String certificateid = CertificateIdField.getText();
-
+            String courseId = CourseIdField.getText();
+            String registration = RegistrationField.getText();
+            String grade = GradeField.getText();
+            String teacher = TeacherField.getText();
             try {
                 Connection connection = DatabaseConnectionManager.getInstance().getConnection();
                 PreparedStatement query = connection.prepareStatement(
-                        "INSERT INTO CourseEnrolled (Email, RegistrationDate, Id, CertificateId) VALUES (?, ?, ?, ?)");
-
+                        "INSERT INTO Certificate (PersonEmail, CourseId, RegistrationDate, Grade, Teacher) VALUES (?, ?, ?, ?, ?)");
+    
                 // Explained: Sets the values for the insert query
                 query.setObject(1, email);
-                query.setString(2, registrationDate);
-                query.setString(3, id);
-                query.setString(4, certificateid);
+                query.setString(2, courseId);
+                query.setString(3, registration);
+                query.setString(4, grade);
+                query.setString(5, teacher);
                 int rowsAffected = query.executeUpdate();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
 
-            // second layout
+            //second layout
             BorderPane layout2 = new BorderPane();
-            Button BackAfterAdd = new Button("go back");
-            ;
-            Label confirmation = new Label(
-                    "entered into database: Email = " + email + ", RegistrationDate = " + registrationDate + ", Id = "
-                            + id + ", CertificateId = " + certificateid);
+            Button BackAfterAdd = new Button("go back");;
+            Label confirmation = new Label("entered into database: Email = " + email + ", CourseId = " + courseId + ", Registration = " + registration + ", Grade = " + grade + ", Teacher = " + teacher);
             vBox2.getChildren().addAll(confirmation, BackAfterAdd);
             BackAfterAdd.setOnAction((event2) -> {
-                layout.setCenter(AddEnrollment.getView());
+                layout.setCenter(AddCertificate.getView());
             });
             scrollPane.setContent(vBox2);
             layout2.setCenter(scrollPane2);
-        });
+        }); 
 
+        
         scrollPane.setContent(vBox);
-        scrollPane.setStyle("-fx-background: #383838; -fx-border-color: red;");
+        scrollPane.setStyle("-fx-background: #383838; -fx-border-color: red;"); 
 
         return layout;
     }
--- src/main/java/dmt/UI/AddUI.java
+++ src/main/java/dmt/UI/AddUI.java
@@ -1,6 +1,5 @@
 package dmt.UI;
 
-
 import dmt.Course;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -18,42 +17,45 @@ import javafx.stage.Stage;
 public class AddUI {
     public static Parent getView() {
 
-        
         AddCourse addCourse = new AddCourse();
         AddEnrollment addEnrollment = new AddEnrollment();
-        
+
         ScrollPane scrollPane = new ScrollPane();
         BorderPane layout = new BorderPane();
         layout.setCenter(scrollPane);
 
         VBox vBox = new VBox();
 
-        //add each button
-            Button AddPersonButton = new Button("Add a Person");;
-            vBox.getChildren().add(AddPersonButton);
-            AddPersonButton.setStyle("-fx-background-color: blue; -fx-font-size: 12px; -fx-text-fill: white");
-            Button AddCourseButton = new Button("Add a course");;
-            vBox.getChildren().add(AddCourseButton);
-            AddCourseButton.setStyle("-fx-background-color: blue; -fx-font-size: 12px; -fx-text-fill: white");
-            Button AddEnrollmentButton = new Button("Add a course enrollment");;
-            vBox.getChildren().add(AddEnrollmentButton);
-            AddEnrollmentButton.setStyle("-fx-background-color: blue; -fx-font-size: 12px; -fx-text-fill: white");
-            Button AddCertificateButton = new Button("Add a certificate");;
-            vBox.getChildren().add(AddCertificateButton);
-            AddCertificateButton.setStyle("-fx-background-color: blue; -fx-font-size: 12px; -fx-text-fill: white");
-            
-            AddPersonButton.setOnAction((event) -> {
-                layout.setCenter(AddPerson.getView());
-            });
-            AddCourseButton.setOnAction((event) -> {
-                layout.setCenter(AddCourse.getView());
-            });
-             AddEnrollmentButton.setOnAction((event) -> {
-                 layout.setCenter(AddEnrollment.getView());
-             });
-            // AddCertificateButton.setOnAction((event) -> {
-            //     layout.setCenter(AddCourse.getView());
-            // });
+        // add each button
+        Button AddPersonButton = new Button("Add a Person");
+        ;
+        vBox.getChildren().add(AddPersonButton);
+        AddPersonButton.setStyle("-fx-background-color: blue; -fx-font-size: 12px; -fx-text-fill: white");
+        Button AddCourseButton = new Button("Add a course");
+        ;
+        vBox.getChildren().add(AddCourseButton);
+        AddCourseButton.setStyle("-fx-background-color: blue; -fx-font-size: 12px; -fx-text-fill: white");
+        Button AddEnrollmentButton = new Button("Add a course enrollment");
+        ;
+        vBox.getChildren().add(AddEnrollmentButton);
+        AddEnrollmentButton.setStyle("-fx-background-color: blue; -fx-font-size: 12px; -fx-text-fill: white");
+        Button AddCertificateButton = new Button("Add a certificate");
+        ;
+        vBox.getChildren().add(AddCertificateButton);
+        AddCertificateButton.setStyle("-fx-background-color: blue; -fx-font-size: 12px; -fx-text-fill: white");
+
+        AddPersonButton.setOnAction((event) -> {
+            layout.setCenter(AddPerson.getView());
+        });
+        AddCourseButton.setOnAction((event) -> {
+            layout.setCenter(AddCourse.getView());
+        });
+        AddEnrollmentButton.setOnAction((event) -> {
+            layout.setCenter(AddEnrollment.getView());
+        });
+        AddCertificateButton.setOnAction((event) -> {
+            layout.setCenter(AddCertificate.getView());
+        });
 
         scrollPane.setContent(vBox);
         scrollPane.setStyle("-fx-background: #383838; -fx-border-color: red;");
@@ -62,4 +64,3 @@ public class AddUI {
         return layout;
     }
 }
-
